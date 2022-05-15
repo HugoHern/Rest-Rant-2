fbfaa2360cfcbc29a6cf580308993c4301a2479b
HugoHern <hugo8954@gmail.com>
2022-05-15 16:40:23 -0400

    adding 404

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//main page
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+//404 PAGE
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404</h1>')
+})
+
+app.listen(process.env.PORT)
